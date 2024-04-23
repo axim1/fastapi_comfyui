6942f9d43da7d3760f1eddf63ceb0ec7cfaae13a
unknown <axim.211@gmail.com>
2024-04-24 00:07:59 +0500

    static for error

--- api/server.py
+++ api/server.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI, BackgroundTasks, Depends
 from api.router import router
 from api.request import request_router
+from fastapi.middleware.cors import CORSMiddleware
 
 def create_app():
     app = FastAPI()
     app.include_router(router)
     app.include_router(request_router)
     app.redirect_slashes = False
+    # Allow requests from all origins
+    app.add_middleware(
+        CORSMiddleware,
+    allow_origins=["*"],  # This should be restricted to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     return app
 
 app = create_app()
